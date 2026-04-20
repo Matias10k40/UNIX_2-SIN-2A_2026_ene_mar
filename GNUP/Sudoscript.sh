65561f2d53a3c1b75627f660a6e37274e23b4d28
Matias10k40 <maquinterosye@uide.edu.ec>
2026-04-20 15:07:19 +0000

    f

--- GNUP/Sudoscript.sh
+++ GNUP/Sudoscript.sh
@@ -9,3 +9,24 @@ sudo sh -c 'echo "chao" >> /etc/archivo_protegido'
 
 # Enters a temporary root session with sudo -i (interactive login); type 'exit' to return.
 sudo -i
+
+# Prints the expanded value of the $HOME variable (user's home directory).
+echo "$HOME"
+
+# Prints the literal string '$HOME' without expanding the variable.
+echo '$HOME'
+
+# Prints the expanded value of the $BASH variable (path to the bash executable).
+echo "$BASH"
+
+# Creates a new file 'hola.sh' and writes the shebang line for a shell script.
+echo '#!/bin/sh'>hola.sh
+
+# Appends the echo command to the hola.sh script file.
+echo 'echo "Hola desde mi primer script"' >> hola.sh
+
+# Displays the contents of the hola.sh file.
+cat hola.sh
+
+# Executes the hola.sh script.
+./hola.sh
