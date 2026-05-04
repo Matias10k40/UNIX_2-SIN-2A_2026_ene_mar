6507a0b195e2c0535482e432826a770b896c0f88
Matias10k40 <maquinterosye@uide.edu.ec>
2026-05-04 02:10:56 +0000

    f

---
+++ GNUP/script_linux_cisco_1.sh
@@ -0,0 +1,29 @@
+1. `ls` - Lists files in a directory
+2. `ls -l` - Lists files with details (permissions, size, owner, etc.)
+3. `ls -t` - Sorts files by timestamp
+4. `ls -s` - Sorts files by size
+5. `ls -r` - Reverses the listing order
+6. `ls -rl` / `ls -lr` - Combines multiple options
+7. `pwd` - Prints the current working directory
+8. `cd [path]` - Changes the current directory
+9. `cd ..` - Goes up to the parent directory
+10. `cd ~` - Goes to the home directory
+11. `cd /` - Goes to the root directory
+12. `su [user]` - Switches to another user by creating a new shell
+13. `su -l` - Performs a full login as another user
+14. `sudo [command]` - Runs a command with root privileges
+15. `ls -l [file]` - Displays the permissions of a file
+16. `chmod [SET][ACTION][PERMISSION] file` - Changes file or directory permissions
+17. `chown [OPTIONS] [OWNER] file` - Changes the owner of a file or directory
+18. `cat [file]` - Displays the full content of a file
+19. `more [file]` - Displays file content one page at a time
+20. `less [file]` - Like more but with additional navigation options
+21. `head [file]` - Displays the first 10 lines of a file
+22. `tail [file]` - Displays the last 10 lines of a file
+23. `head -n [N] file` - Displays the first N lines of a file
+24. `tail -n [N] file` - Displays the last N lines of a file
+25. `cp [SOURCE] [DESTINATION]` - Copies a file
+26. `mv [SOURCE] [DESTINATION]` - Moves or renames a file
+27. `rm [file]` - Removes a file
+28. `rm -r [directory]` - Removes a directory recursively
+29. `dd [OPTIONS] OPERAND` - Copies files or partitions at the bit level
