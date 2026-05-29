4c21be6185efa23bb9a5aa5aa323302bc8d5b271
Matias10k40 <maquinterosye@uide.edu.ec>
2026-05-29 15:08:38 +0000

    security2

---
+++ Dockerfile/security.zsh
@@ -0,0 +1,8 @@
+id 
+# See the main group
+id -gn # Name of the lead group
+# Create one file and see the group
+touch ~/test_grupo_heredado.txt
+ls -la  ~/test_grupo_heredado.txt
+#The grup is the gruop principal of the user
+
