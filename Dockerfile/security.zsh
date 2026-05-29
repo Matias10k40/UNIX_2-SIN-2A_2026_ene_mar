c8379caab3a7a0f4cd8802e9b3c1845f7256e1e5
Matias10k40 <maquinterosye@uide.edu.ec>
2026-05-29 15:25:46 +0000

    s2.3

--- Dockerfile/security.zsh
+++ Dockerfile/security.zsh
@@ -5,9 +5,13 @@ id -gn # Name of the lead group
 touch ~/test_grupo_heredado.txt
 ls -la  ~/test_grupo_heredado.txt
 #The grup is the gruop principal of the user
+groups
+# Create a new group and add the user to it
 newgrp desarrollo
 # See the new group
 id -gn
 # Create one file and see the group
-echo "Nuevo grupo activo: $(id -gn)"
+tocuh ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
+# Create one file and see the group
 
