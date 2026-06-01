d89cf03b09a8ac204c9387737f947dff89ded5c0
Matias10k40 <maquinterosye@uide.edu.ec>
2026-06-01 15:28:00 +0000

    blaclhatbash3

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 set -x
-bash --version 
+bash --version
 env
-echo ${SHELL}
-echo ${RANDOM}
-echo ${UID}
-echo ${OSTYPE}
+echo "${SHELL}"
+echo "${RANDOM}"
+echo "${UID}"
+echo "${OSTYPE}"
 ps -e -f
 ps -ef
 df --human-readable
-bash -r blackhatbash1.sh  
-echo "Hello World!" 
-bash helloworld.sh
-bash -n script.sh
-bash -x script.sh
-set +x 
+echo "Hello World!"
+# Para ejecutar otro script usa la ruta del archivo:
+# ./helloworld.sh
+# ./script.sh
+# Para verificar sintaxis sin ejecutar usa:
+# bash -n script.sh
+set +x
+# + df --human-readable
+# Filesystem      Size  Used Avail Use% Mounted on
+# overlay          32G   13G   18G  42% /
+# tmpfs            64M     0   64M   0% /dev
+# shm              64M     0   64M   0% /dev/shm
+# /dev/root        29G   23G  6.3G  79% /vscode
+# /dev/sdb1        44G  2.8G   39G   7% /tmp
+# /dev/loop4       32G   13G   18G  42% /workspaces
+# + echo 'Hello World!'
+# Hello World!
+# + set +x
