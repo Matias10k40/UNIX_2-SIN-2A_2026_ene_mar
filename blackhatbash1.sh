3a5f5bad89c339a0005037729b143da17c4cf72b
Matias10k40 <maquinterosye@uide.edu.ec>
2026-06-01 15:05:59 +0000

    blackhatbash2

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -4,4 +4,11 @@ echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
- 
+ps -e -f
+ps -ef
+df --human-readable
+#!/bin/bash
+#!/usr/bin/env bash
+#!/bin/bash -x 
+#!/bin/bash -r restricted
+ bash -r script.sh
