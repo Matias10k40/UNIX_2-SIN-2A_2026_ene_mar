e29098e6a23a9e042cedffaf549a166125db1469
Matias10k40 <maquinterosye@uide.edu.ec>
2026-06-05 15:28:29 +0000

    byeword

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,4 +3,7 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "fallo el comando lzl"
-
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
