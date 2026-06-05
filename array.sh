e510960d8191ff5eac44b4ab5c6e915d0389efc8
Matias10k40 <maquinterosye@uide.edu.ec>
2026-06-05 14:54:28 +0000

    array2

--- array.sh
+++ array.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -x
 # Sets an array 
 IP_ADDRESSES=(192.168.1.1 .192.168.1.2 192.168.1.3)
+# Prints the first element of the array
+set -x
+echo "${IP_ADDRESSES[*]}"
 set +x
+
+echo "${IP_ADDRESSES[0]}"
+#
+#echo "${IP_ADDRESSES[*]}"
+IP_ADDRESSES[0]="192.168.1.10"
+
+
+
+
+
