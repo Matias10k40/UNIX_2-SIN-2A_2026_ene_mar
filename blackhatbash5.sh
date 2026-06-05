071dd4722e3ed8b8ba98fa06ed02e0e5292172d1
Matias10k40 <maquinterosye@uide.edu.ec>
2026-06-05 15:38:14 +0000

    blackhatbash5.1

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -7,3 +7,8 @@ echo "Hello World!" > output.txt
 cat output.txt
 echo "Bye World!" > output.txt
 cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
